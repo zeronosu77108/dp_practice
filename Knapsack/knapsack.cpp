ae677c4947a35be3a67c02c05017392c573257e1
zeronosu77108 <taiga77108@gmail.com>
2018-12-30 13:41:39 +0900

    add knapsack problem

---
+++ Knapsack/knapsack.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int N,W;    // 物の個数，最大の重さ
+    int weight[110], value[110];
+
+    int dp[110][10010];     // dp table
+
+    // 入力
+    cin >> N >> W;
+    for (int i=0; i<N; i++) cin >> value[i] >> weight[i];
+
+    // DP
+    for (int i=0; i<N; i++) {
+        for (int w=0; w<=W; w++) {
+            // i 番目の物が， w （一時的な容量）を超えないなら，入れる
+            // そうでないなら，前と同じ．
+            if( w>= weight[i]) {
+                dp[i+1][w] = dp[i][w - weight[i]] + value[i];
+            } else {
+                dp[i+1][w] = dp[i][w];
+            }
+        }
+    }
+
+    cout << dp[N][W] << endl;
+}
