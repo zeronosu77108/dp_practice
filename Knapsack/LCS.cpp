2c84a2fbf49f655f6acb84f22771925f1a4890fa
zeronosu77108 <taiga77108@gmail.com>
2018-12-31 22:54:52 +0900

    Add comment at LCS.cpp

--- Knapsack/LCS.cpp
+++ Knapsack/LCS.cpp
@@ -1,3 +1,14 @@
+/* 最長共通部分列問題（LCS）
+ *   2つの文字列(s,t)が与えられたとき，
+ *   共通の部分文字列の長さの最大値を求める問題
+ *
+ * dp[i][j] は，
+ * s の i 文字目， t の j 文字目まだでの共通部分文字列
+ * 
+ * s[i] と t[i] が等しいなら， dp[i+1][j+1] = dp[i][j] + 1
+ * そうでないなら，
+ *   dp[i+1][j+1] = max( dp[i][j+1] , dp[i+1][j] )
+ */
 #include <iostream>
 #include <string>
 #include <algorithm>
